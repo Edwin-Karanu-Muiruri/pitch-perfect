dca0722adf48ffb3c305baf3f1af56656e679955
Edwin Karanu Muiruri <edwin.karanu.muiruri@gmail.com>
2020-05-03 01:31:00 +0300

    Added bootstrap to the application for styling purposes.

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from .config import DevConfig
+from flask_bootstrap import Bootstrap
 #initializing the application
 app = Flask(__name__)
 
+#where we setup the app configurations
 app.config.from_object(DevConfig)
+
+#where I initialize flask extensions
+bootstrap = Bootstrap(app)
 
 from app import views

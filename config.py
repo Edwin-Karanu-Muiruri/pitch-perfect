be31358e120e5e5357c3c8532ce1778f49bd786e
Edwin Karanu Muiruri <edwin.karanu.muiruri@gmail.com>
2020-05-05 06:00:02 +0300

    Created the database,and the User and Roles classes.

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ class Config:
     General configuration parent class
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://edwin:EDUcaranow98@localhost/pitchperfect'
 
 class ProdConfig(Config):
     '''
--- manage.py
+++ manage.py
@@ -1,10 +1,15 @@
 from app import create_app,db
-from app import app
-from app.models import User
+from flask_script import Manager,Server
+from app.models import User,Role
+
+app = create_app('development')
+
+manager = Manager(app)
+manager.add_command('server',Server)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User)
+    return dict(app = app,db = db,User = User, Role = Role)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
